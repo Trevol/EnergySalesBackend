405e0559466ab2f19af3ac3af8c08cf7ec4d05a1
Vladimir Tretyak <trvo@smart-dn.ru>
2021-08-21 08:58:59 +0300

    calc backendRootUri using location of ui assets (share same host? but use different ports for ui and api)

--- frontend/src/js/appSettings.js
+++ frontend/src/js/appSettings.js
@@ -1,5 +1,6 @@
 // const backendRootUri = "http://192.168.87.8:8080"
-const backendRootUri = "http://0.0.0.0:8080"
+const backendPort = 8080
+const backendRootUri = `${window.location.protocol}//${window.location.hostname}:${backendPort}`
 
 export {
     backendRootUri
