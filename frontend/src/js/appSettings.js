fc761c5c15c0d1b1f9935b2c76588be621cfa450
Vladimir Tretyak <trvo@smart-dn.ru>
2021-08-21 08:59:22 +0300

    calc backendRootUri using location of ui assets (share same host? but use different ports for ui and api)

--- frontend/src/js/appSettings.js
+++ frontend/src/js/appSettings.js
@@ -1,5 +1,6 @@
 // const backendRootUri = "http://192.168.87.8:8080"
 const backendPort = 8080
+//https://stackoverflow.com/questions/1368264/how-to-extract-the-hostname-portion-of-a-url-in-javascript
 const backendRootUri = `${window.location.protocol}//${window.location.hostname}:${backendPort}`
 
 export {
