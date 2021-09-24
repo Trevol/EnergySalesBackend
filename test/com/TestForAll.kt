b1d0da17a256ae072efe2b0ad74e30da9181f1e8
Vladimir Tretyak <trvo@smart-dn.ru>
2021-09-24 15:07:11 +0300

    Распределение 2021 08 август.xlsx

--- test/com/TestForAll.kt
+++ test/com/TestForAll.kt
@@ -10,6 +10,8 @@ import kotlinx.serialization.Serializable
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 import org.junit.Test
+import java.time.*
+import java.time.format.DateTimeFormatter
 import kotlin.random.Random
 
 class TestForAll {
@@ -20,5 +22,32 @@ class TestForAll {
         m[MonthOfYear(4, 2121)] = 123.45
         m.println()
         m[MonthOfYear(4, 2121)].println()
+    }
+
+    @Test
+    fun mapPlusMap() {
+        val map1 = mapOf(1 to 11, 2 to 22)
+        val map2 = mapOf(4 to 44, 3 to 33)
+
+        (map1 + map2) shouldBe mapOf(1 to 11, 2 to 22, 4 to 44, 3 to 33)
+        (map1 + map2).println()
+    }
+
+    @Test
+    fun parseInstant() {
+        // val dt = DateTimeFormatter.ofPattern("dd.MM.yy HH:mm:ss").parse("20.09.21 20:00:00")
+        val temporal = DateTimeFormatter.ofPattern("dd.MM.yy HH:mm:ss:SSS").parse("20.09.21 20:00:00:002")
+        temporal.println()
+        val dt = LocalDateTime.from(temporal).toInstant(ZoneOffset.UTC)
+        Instant.from(dt).println()
+    }
+
+    @Test
+    fun formatDateTime(){
+        val formatter = DateTimeFormatter.ofPattern("dd.MM.yy HH:mm")
+        val formatted = formatter.format(LocalDateTime.now())
+        formatted.println()
+        formatter.parse(formatted)
+            .println()
     }
 }
